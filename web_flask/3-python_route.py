f0581e0ac34ab6b7a3296761a0ce0509c9642622
twizelissa <twizelissa@gmail.com>
2024-10-12 12:01:17 +0400

    add task 3,3 and refactor 0

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/',strict_slashes=False)
+def hello_hbnb():
+    return "Hello, HBNB!"
+
+@app.route('/hbnb',strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>',strict_slashes=False)
+def c_text(text):
+    text = text.replace('_',' ')
+    return f"C {text}"
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text=None):
+ 
+    if text is None:
+        text = "is cool"
+    
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000,debug=True)
